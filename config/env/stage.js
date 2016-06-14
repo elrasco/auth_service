71df120d62c9d7c07a9cf8619243aafc464a2e8a
Shy Alter <puemos@gmail.com>
2016-06-14 16:48:18 +0200

    const cluster_private_ip = process.env.CLUSTER_PRIVATE_IP;

--- config/env/stage.js
+++ config/env/stage.js
@@ -9,33 +9,34 @@
  * any private information to this file!
  *
  */
+const cluster_private_ip = process.env.CLUSTER_PRIVATE_IP;
 
 module.exports = {
 
-  /***************************************************************************
-   * Set the default database connection for models in the production        *
-   * environment (see config/connections.js and config/models.js )           *
-   ***************************************************************************/
+    /***************************************************************************
+     * Set the default database connection for models in the production        *
+     * environment (see config/connections.js and config/models.js )           *
+     ***************************************************************************/
 
-  models: {
-    connection: 'smallfish_auth',
-    migrate: 'safe'
-  },
+    models: {
+        connection: 'smallfish_auth',
+        migrate: 'safe'
+    },
 
-  port: 1339,
-  
-  /***************************************************************************
-   * Set the port in the production environment to 80                        *
-   ***************************************************************************/
+    port: 1339,
 
-  // port: 80,
+    /***************************************************************************
+     * Set the port in the production environment to 80                        *
+     ***************************************************************************/
 
-  /***************************************************************************
-   * Set the log level in production environment to "silent"                 *
-   ***************************************************************************/
+    // port: 80,
 
-  log: {
-    level: "silent"
-  }
+    /***************************************************************************
+     * Set the log level in production environment to "silent"                 *
+     ***************************************************************************/
+
+    log: {
+        level: "silent"
+    }
 
 };
