d2ad382b21692ac783690c0f8e307ef68056be92
Luca Rasconi <luca.rasconi@gmail.com>
2016-06-29 11:21:13 +0200

    fix Function.generateRandomString

--- api/services/Functions.js
+++ api/services/Functions.js
@@ -5,7 +5,7 @@ module.exports.generateRandomString = function(len){
 
 	var length = len || password_default_length;
 	var charset = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    retVal = '';
+    let retVal = '';
     for (var i = 0, n = charset.length; i < length; ++i) {
         retVal += charset.charAt(Math.floor(Math.random() * n));
     }
