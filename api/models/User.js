37b817b0a6a3d11f5d101918df9dc910f39ae916
Luca Rasconi <luca.rasconi@gmail.com>
2016-12-21 15:56:53 +0100

    getter password null

--- api/models/User.js
+++ api/models/User.js
@@ -18,7 +18,7 @@ module.exports = {
             type: Sequelize.STRING,
             protected: true,
             get: function() {
-              return '';
+              return null;
             }
         },
         firstPassword: Sequelize.STRING,
